8c734b6a5440af0acb033edc2fffae838c73de6f
salehfaraj08 <saleh.faraj08@gmail.com>
2021-11-15 13:36:36 +0200

    update

--- routes/library.route.js
+++ routes/library.route.js
@@ -2,15 +2,15 @@ const express = require('express');
 const libraryController = require('../controllers/library.controller');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api/library', (req, res) => {
     libraryController.getAllBooks(req, res);
-}).post('/', (req, res) => {
+}).post('/api/library', (req, res) => {
     libraryController.createBook(req, res);
-}).get('/:year', (req, res) => {
+}).get('/api/library/:year', (req, res) => {
     libraryController.getBookByYear(req, res);
-}).delete('/:deleteId', (req, res) => {
+}).delete('/api/library/:deleteId', (req, res) => {
     libraryController.deleteBook(req, res);
-}).put('/:updateId', (req, res) => {
+}).put('/api/library/:updateId', (req, res) => {
     libraryController.updateBook(req, res);
 });
 
